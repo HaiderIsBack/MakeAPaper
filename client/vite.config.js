10a0698ef75760590c178e0898fadda67e883fa4
HaiderIsBack <haidershahpak17@gmail.com>
2024-07-07 04:55:49 -0700

    changing frontends server URLs

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api/v1":"http://localhost:8080"
+      "/api/v1": "https://test-builder-iota.vercel.app"
     }
   },
   plugins: [react()],
