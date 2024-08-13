d1f864a7b41cd051f5b57e4c542377c8130dda9a
HaiderIsBack <haidershahpak17@gmail.com>
2024-08-13 04:36:24 -0700

    checking fix

--- admin/frontend/vite.config.js
+++ admin/frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": "https://test-builder-iota.vercel.app"
-    }
-  },
   plugins: [react()],
 })
